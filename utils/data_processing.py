81be8127e56f8eba9aac83badc2bef26662c24af
Simon Luder <simon.luder@students.fhnw.ch>
2024-07-18 15:25:31 +0200

    Update data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,6 +1,66 @@
+import os
 import cv2
 import skimage
 import numpy as np
+from tqdm import tqdm
+
+def recalculate_holographic_features(df, image_path):
+    """
+    Recalculates holographic features for each row in the DataFrame.
+
+    Parameters:
+    - df (pandas.DataFrame): Input DataFrame containing relevant data.
+    - image_path (str): Path to the directory containing images.
+
+    Returns:
+    - pandas.DataFrame: Updated DataFrame with recalculated features.
+    """
+
+    df = df.copy()
+
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+
+        img_path = os.path.join(image_path, row["dataset_id"], row["rec_path"])
+
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+        if img is not None:
+            rp = regionprops_from_numpy(img)
+            df.at[i, 'area']                  = rp[0].area
+            df.at[i, 'bbox_area']             = rp[0].bbox_area
+            df.at[i, 'convex_area']           = rp[0].convex_area
+            df.at[i, 'major_axis_length']     = rp[0].major_axis_length
+            df.at[i, 'minor_axis_length']     = rp[0].minor_axis_length
+            df.at[i, 'eccentricity']          = rp[0].eccentricity
+            df.at[i, 'solidity']              = rp[0].solidity
+            df.at[i, 'perimeter']             = rp[0].perimeter
+            df.at[i, 'perimeter_crofton']     = rp[0].perimeter_crofton
+            df.at[i, 'equivalent_diameter']   = rp[0].equivalent_diameter
+            df.at[i, 'orientation']           = rp[0].orientation
+            df.at[i, 'feret_diameter_max']    = rp[0].feret_diameter_max
+            df.at[i, 'max_intensity']         = rp[0].intensity_max[0]
+            df.at[i, 'min_intensity']         = rp[0].intensity_min[0]
+            df.at[i, 'mean_intensity']        = rp[0].intensity_mean[0]
+
+        else:
+            df.at[i, 'area']                  = None
+            df.at[i, 'bbox_area']             = None
+            df.at[i, 'convex_area']           = None
+            df.at[i, 'major_axis_length']     = None
+            df.at[i, 'minor_axis_length']     = None
+            df.at[i, 'eccentricity']          = None
+            df.at[i, 'solidity']              = None
+            df.at[i, 'perimeter']             = None
+            df.at[i, 'perimeter_crofton']     = None
+            df.at[i, 'equivalent_diameter']   = None
+            df.at[i, 'orientation']           = None
+            df.at[i, 'feret_diameter_max']    = None
+            df.at[i, 'max_intensity']         = None
+            df.at[i, 'min_intensity']         = None
+            df.at[i, 'mean_intensity']        = None
+
+    return df
+
 
 
 def convert_pixel_to_um(df, resolution):
